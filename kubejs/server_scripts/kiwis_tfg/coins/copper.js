6502dd786b0068d3442fe56593597cce6b951eb7
Mavikedi <ozandemir2007@gmail.com>
2025-12-29 20:04:15 +0300

    full update to 0.11.12 with same changes as the server

---
+++ kubejs/server_scripts/kiwis_tfg/coins/copper.js
@@ -0,0 +1,26 @@
+ItemEvents.crafted((event) => {
+    
+    if (event.item.id === 'createdeco:copper_coin') {
+        const player = event.player;
+        if (player) {
+        
+            event.item.nbt = event.item.nbt ?? {};
+            event.item.nbt.creator = player.username;
+        }
+    }
+    if (event.item.id === 'createdeco:copper_coin') {
+        let player = event.player;
+        if (!player) return;
+
+        let item = event.item;
+        if (!item) return;
+
+        let pos = player.blockPosition();
+        let chunkX = Math.floor(pos.x / 16);
+        let chunkZ = Math.floor(pos.z / 16);
+    
+            event.item.nbt = event.item.nbt ?? {};
+            item.nbt.craftedChunkX = chunkX;
+            item.nbt.craftedChunkZ = chunkZ;
+        }
+});
